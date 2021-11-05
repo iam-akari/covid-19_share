e523faa3d2c5390c5e1cc9db98573b547a084bc4
iam-akari <77928363+iam-akari@users.noreply.github.com>
2021-11-05 13:45:28 +0900

    Add files via upload

---
+++ Views/Posts/suv_footer.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" type="text/css" href="css/post.css">
+<div class="footer2">
+<footer>
+<body>
+    <p><small>© 2021- covid-19 share site </small></p>
+</footer>
+</div><!--class="footer2"-->
+
+</body>
+</html>
